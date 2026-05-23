8a40c2df554076efe01a47e9727a8cb8cef7805e
Kenechukwu Jason Akubue <kenechukwu.akubue@pau.edu.ng>
2026-05-23 15:36:10 +0100

    Created theme.py for UI themes

---
+++ app/themes.py
@@ -0,0 +1,10 @@
+#The themes are constants used across files for UI design
+APP_BG_CLR = "#000000"
+APP_TITLE_TXT_CLR = "#AFB8CB"
+APP_TXT_CLR = "#FFFFFF"
+BTN_BG_CLR = "#190bda"
+BTN_TXT_CLR = "#FFFFFF"
+ENTRY_BG_CLR = "#353535"
+ENTRY_TXT_CLR = "#000000"
+
+TITLE_FONT = ("Arial", 24, "bold")
